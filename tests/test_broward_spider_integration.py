f7c633a88fde18581ee8cf09d02ef70d41d2fdc8
tschmidt95 <134100264+tschmidt95@users.noreply.github.com>
2026-01-03 13:30:48 -0500

    test(spiders): shorten long spider import lines by using package alias

--- tests/test_broward_spider_integration.py
+++ tests/test_broward_spider_integration.py
@@ -2,7 +2,9 @@ from pathlib import Path
 from urllib.request import pathname2url
 
 from scrapy.http import TextResponse
-from florida_property_scraper.backend.spiders.broward_spider import BrowardSpider
+from florida_property_scraper.backend import spiders as spiders_pkg
+
+BrowardSpider = spiders_pkg.broward_spider.BrowardSpider
 
 
 def test_broward_spider_collects_items():
